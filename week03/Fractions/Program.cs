f2a5c525a878aecfa3f9e5b888840b2fc40a13c6
jorgevaoli <val23027@byui.edu>
2025-03-26 23:04:49 -0500

    W03 Learning Activity: Encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -2,8 +2,23 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction frac1 = new Fraction(); 
+        Fraction frac2 = new Fraction(5); 
+        Fraction frac3 = new Fraction(3, 4); 
+        Fraction frac4 = new Fraction(1, 3); 
+
+        Console.WriteLine(frac1.GetFractionString()); 
+        Console.WriteLine(frac1.GetDecimalValue());   
+
+        Console.WriteLine(frac2.GetFractionString()); 
+        Console.WriteLine(frac2.GetDecimalValue());   
+
+        Console.WriteLine(frac3.GetFractionString()); 
+        Console.WriteLine(frac3.GetDecimalValue());   
+
+        Console.WriteLine(frac4.GetFractionString()); 
+        Console.WriteLine(frac4.GetDecimalValue());  
     }
 }
